bf1b4915d3a15ca91f40a93814f078a2578a1df8
CarlosFrancoFerrer <76023119+CarlosFrancoFerrer@users.noreply.github.com>
2020-12-15 12:24:10 -0600

    Add files via upload

---
+++ js.js
@@ -0,0 +1,12 @@
+//alert("Bienvenido");
+var boleta, nombre, grupo, materia, fecha;
+boleta=prompt("Ingrese su boleta:");
+nombre=prompt("Ingrese su nombre:");
+grupo=prompt("Ingrese su grupo:");
+materia=prompt("Ingrese su materia:");
+fecha=prompt("Ingrese alguna fecha:");
+alert(`Su boleta es: ${boleta}`);
+alert(`Su nombre es: ${nombre}`);
+alert(`Su grupo es: ${grupo}`);
+alert(`Su materia es: ${materia}`);
+alert(`La fecha que ingreso es: ${fecha}`);
